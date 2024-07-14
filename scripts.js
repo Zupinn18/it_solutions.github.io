dad0c36f8316497599925ac9a03e72e400682981
shivam malhotra <141710462+Zupinn18@users.noreply.github.com>
2024-07-14 12:59:28 +0530

    u

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 // JavaScript code to cycle through images every second
 document.addEventListener('DOMContentLoaded', () => {
     const images = [
-        "assets/bg_image/6386.jpg_wh860.jpg",
-        "assets/bg_image/social-media-business-intelligence.jpg", // Add your second image path here
-        "assets/bg_image/pngtree-hand-gesture-business-team-cooperation-win-win-photograph-with-picture-business-image_7.jpg"  // Add your third image path here
+        "assets/bg_image/resiged/9.jpg",
+        "assets/bg_image/resiged/24.jpg", // Add your second image path here
+        "assets/bg_image/resiged/17.jpg"  // Add your third image path here
     ];
     let currentIndex = 0;
     const imageElement = document.getElementById('card-image');
